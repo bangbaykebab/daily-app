9b754a446002e125c51bfc59844aed79ac115ac5
kebab <bangbaykebab@gmail.com>
2025-07-30 12:38:21 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,36 @@
-const CACHE_NAME = 'daily-routine-cache-v1';
+const CACHE_NAME = "daily-routine-cache-v1";
 const urlsToCache = [
-  'index.html',
-  'manifest.json',
-  'service-worker.js',
-  'icons/icon-192.png',
-  'icons/icon-512.png'
+  "./",
+  "index.html",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => 
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
   );
 });
